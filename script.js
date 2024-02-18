732742d37b3c627501470fbd20c77b0d4c0a8744
Sahadev1431 <naisahadev@gmail.com>
2024-02-18 15:46:45 +0530

    :)

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function firstPageAnim()
         y: '-10',
         opacity :0,
         duration : 1.3,
+        delay : 1,
         ease: Expo.easeInout
     })
 
@@ -33,6 +34,50 @@ function firstPageAnim()
         delay : -1,
         stagger:.2
     })
+
+    .from(".herofooter",
+    {
+        y:-10,
+        opacity:0,
+        ease:Expo.easeInout,
+        duration : .9,
+        delay : -.7
+    })
 }
 
 firstPageAnim();
+
+
+let elements = document.querySelectorAll(".elem");
+
+elements.forEach(function(eleme)
+{
+   
+
+    let rotate = 0;
+    let diffRot= 0;
+    
+    eleme.addEventListener("mousemove",function(dets)
+    {
+        let diff = (dets.clientY-eleme.getBoundingClientRect().top);
+
+        diffRot = dets.clientX - rotate;
+        rotate = dets.clientX;
+
+        gsap.to(eleme.querySelector("img"),{
+            opacity:1,
+            ease : Power3,
+            top: diff,
+            left:dets.clientX,
+            rotate: gsap.utils.clamp(-20,20,diffRot)
+        })
+    })
+
+    eleme.addEventListener("mouseleave",function(dets)
+    {
+        gsap.to(eleme.querySelector("img"),{
+            opacity:0,
+            ease : Power3
+        })
+    })
+})
